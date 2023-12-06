97a2fa7b0b9bea4870e328b1485918adcae2bbd5
B00144945 <B00144945@mytudublin.ie>
2023-12-06 10:10:38 +0000

    Fixed layout of the workouts table

--- showWorkouts.php
+++ showWorkouts.php
@@ -10,18 +10,23 @@ $sql = "select * from workouts";
 
 
 $result = mysqli_query($conn,$sql);
+echo "<TABLE border='1'>";
 while($row=mysqli_fetch_assoc($result)) {
+	$fn = $row['firstname'];
+	$sn = $row['surname'];
+	$dob = $row['dateofbirth'];
 	$workout_name = $row['workout_name'];
 	$duration = $row['duration'];
 	$date = $row['date'];	
 	
-	echo "$workout_name, $duration, $date<br>";
+	echo "<TR><TD>$fn</TD><TD>$sn</TD><TD>$dob</TD><TD>$workout_name</TD><TD>$duration</TD><TD>$date</TD></TR>";
 	
 	
 	
 	
 }
 
+echo "</TABLE>";
 
 
 mysqli_close($conn);
--- sql/createDBV1.sql
+++ sql/createDBV1.sql
@@ -2,10 +2,15 @@
 DROP TABLE if EXISTS workouts;
 CREATE TABLE workouts (
     id INT AUTO_INCREMENT, 
+    firstname VARCHAR(30),
+    surname VARCHAR(30),
+    dateofbirth DATE,
     workout_name VARCHAR(30), 
     duration INT, 
     date DATE,
     PRIMARY KEY (id)
 ); 
 
-INSERT INTO workouts (workout_name, duration, date) VALUES ('burpees', '5', '2023-11-29');
+INSERT INTO workouts (firstname, surname, dateofbirth, workout_name, duration, date) VALUES ('Iarlaith', 'OCarroll', '2001-04-01', 'pull ups', '3', '2023-12-01');
+
+SELECT * FROM workouts
